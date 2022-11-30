3f49bbc45f1a48c047bee4f99f04d4cfd38f7bd7
Ahmed Nouh <104303369+AhmedSaidNouh@users.noreply.github.com>
2022-11-30 09:51:16 +0200

    Cumulative Probability

--- InventorySimulation/InventoryModels/SimulationSystem.cs
+++ InventorySimulation/InventoryModels/SimulationSystem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,7 +18,132 @@ namespace InventoryModels
         }
 
         ///////////// INPUTS /////////////
+        /// <summary>
+        /// 
+        /// </summary>
+        public void readFromFile(string filePath, SimulationSystem sumlation)
+        {
 
+            FileStream fileStream = new FileStream(filePath, FileMode.Open);
+            StreamReader reader = new StreamReader(fileStream);
+            while (reader.Peek() != -1)
+            {
+                string lineRead = reader.ReadLine();
+                if (lineRead == "OrderUpTo")
+                {
+                    OrderUpTo= int.Parse(reader.ReadLine());
+                }
+                else if (lineRead == "ReviewPeriod")
+                {
+                    ReviewPeriod = int.Parse(reader.ReadLine());
+                }
+                else if (lineRead == "StartInventoryQuantity")
+                {
+                    StartInventoryQuantity = int.Parse(reader.ReadLine());
+                }
+                else if (lineRead == "StartOrderQuantity")
+                {
+
+                    StartOrderQuantity = int.Parse(reader.ReadLine());
+                }
+                else if (lineRead == "StartLeadDays")
+                {
+
+                    StartLeadDays = int.Parse(reader.ReadLine());
+                }
+                else if (lineRead == "NumberOfDays")
+                {
+                    NumberOfDays = int.Parse(reader.ReadLine());
+                }
+                else if (lineRead == "DemandDistribution")
+                {
+                    
+                    while (true)
+                    {
+                        string intervalLine = reader.ReadLine();
+                        if (intervalLine == "")
+                        {
+                            break;
+                        }
+                        string[] interval_Prob = intervalLine.Split(',');
+                            Distribution row = new Distribution();
+                            row.Probability = decimal.Parse(interval_Prob[1]);
+                        row.Value= int.Parse(interval_Prob[0]);
+                       DemandDistribution.Add(row);
+                 
+                    }
+                    calculateCummProb_demaind();
+                }
+                else if (lineRead == "LeadDaysDistribution")
+                {
+                    while (true)
+                    {
+                        string intervalLine = reader.ReadLine();
+                        if (intervalLine == null)
+                        {
+                            break;
+                        }
+                        string[] interval_Prob = intervalLine.Split(',');
+
+                        Distribution row = new Distribution();
+                        row.Probability = decimal.Parse(interval_Prob[1]);
+                        row.Value = int.Parse(interval_Prob[0]);
+                        LeadDaysDistribution.Add(row);
+
+                    }
+                    calculateCummProb_lead();
+                }
+                //test randoms
+
+            }
+            fileStream.Close();
+        }
+        private void calculateCummProb_demaind()
+        {
+            decimal sumCProb = 0;
+            foreach (Distribution Row in DemandDistribution)
+            {
+                if (sumCProb * 100 == 100||Row.Probability==0)
+                {
+                    Row.MinRange = 0;
+                    Row.MaxRange = 0;
+                }
+                else
+                {
+                   
+                    Row.MinRange = (int)(sumCProb * 100) + 1;
+                    sumCProb += Row.Probability;
+                    Row.CummProbability = sumCProb;
+                    Row.MaxRange = (int)(sumCProb * 100);
+                }
+            }
+        }
+
+        private void calculateCummProb_lead()
+        {
+            decimal sumCProb = 0;
+            foreach (Distribution Row in LeadDaysDistribution)
+            {
+                if (sumCProb * 10 == 10 || Row.Probability == 0)
+                {
+                    Row.MinRange = 0;
+                    Row.MaxRange = 0;
+                }
+                else
+                {
+                    
+                    Row.MinRange = (int)(sumCProb * 10) + 1;
+                    sumCProb += Row.Probability; 
+                    Row.CummProbability = sumCProb;
+                    Row.MaxRange = (int)(sumCProb * 10);
+                    if (Row.MaxRange < Row.MinRange)
+                    {
+                        Row.MinRange = 0;
+                        Row.MaxRange = 0;
+                    }
+                }
+            }
+        }
         public int OrderUpTo { get; set; }
         public int ReviewPeriod { get; set; }
         public int NumberOfDays { get; set; }
--- InventorySimulation/InventorySimulation/Program.cs
+++ InventorySimulation/InventorySimulation/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using InventoryModels;
+using InventoryTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -17,6 +19,22 @@ namespace InventorySimulation
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            SimulationSystem sim = new SimulationSystem();
+            string path = "../../TestCases/TestCase4.txt";
+            sim.readFromFile(path, sim);
+           // string test= TestingManager.Test(sim, Constants.FileNames.TestCase1);
+           
+           /* Console.WriteLine(sim.OrderUpTo);
+            Console.WriteLine(sim.ReviewPeriod);
+            Console.WriteLine(sim.StartInventoryQuantity);
+            Console.WriteLine(sim.StartLeadDays);
+            Console.WriteLine(sim.StartOrderQuantity);
+            Console.WriteLine(sim.NumberOfDays);*/
+            foreach (Distribution row in sim.LeadDaysDistribution)
+            {
+                
+                Console.WriteLine(row.Value + ","+row.Probability+","+row.CummProbability +","+row.MinRange+','+row.MaxRange);
+            }
         }
     }
 }
